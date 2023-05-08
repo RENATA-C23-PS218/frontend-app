2359d1a8e2fba18adac1b1c3a4f10f36ab2d64ce
AudiHagi <audi.hagi2002@gmail.com>
2023-05-08 22:02:52 +0700

    Login With Google

--- app/src/main/java/com/renata/view/fragment/account/AccountFragment.kt
+++ app/src/main/java/com/renata/view/fragment/account/AccountFragment.kt
@@ -6,13 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.renata.databinding.FragmentAccountBinding
+import com.renata.view.activity.login.LoginActivity
 import com.renata.view.activity.setting.SettingActivity
 
 class AccountFragment : Fragment() {
 
     private var _binding: FragmentAccountBinding? = null
     private val accountBinding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +30,16 @@ class AccountFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = Firebase.auth
+        val firebaseUser = auth.currentUser
+        if (firebaseUser == null) {
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+            requireActivity().finish()
+            return
+        }
+
         accountBinding.settingButton.setOnClickListener { goToSetting() }
+        accountBinding.logoutButton.setOnClickListener { signOut() }
     }
 
     override fun onDestroyView() {
@@ -36,6 +50,12 @@ class AccountFragment : Fragment() {
     private fun goToSetting() {
         val intentToSetting = Intent(requireContext(), SettingActivity::class.java)
         startActivity(intentToSetting)
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        startActivity(Intent(requireContext(), LoginActivity::class.java))
+        requireActivity().finish()
     }
 
 }
