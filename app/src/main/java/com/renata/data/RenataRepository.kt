3d496506a519c9f80db6ec43039eea4f5f2e0e23
AudiHagi <audi.hagi2002@gmail.com>
2023-06-05 20:38:07 +0700

    bug fixing classify soil image

--- app/src/main/java/com/renata/data/RenataRepository.kt
+++ app/src/main/java/com/renata/data/RenataRepository.kt
@@ -40,10 +40,7 @@ class RenataRepository(private val application: Application) {
     fun classifyImage(image: Bitmap): String {
         try {
             val model: Model = Model.newInstance(application.applicationContext)
-            val inputFeature0 = TensorBuffer.createFixedSize(
-                intArrayOf(1, imageSize, imageSize, 3),
-                DataType.FLOAT32
-            )
+            val imageSize = 224
             val byteBuffer: ByteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
             byteBuffer.order(ByteOrder.nativeOrder())
             val intValues = IntArray(imageSize * imageSize)
@@ -52,35 +49,27 @@ class RenataRepository(private val application: Application) {
             for (i in 0 until imageSize) {
                 for (j in 0 until imageSize) {
                     val `val` = intValues[pixel++] // RGB
-                    byteBuffer.putFloat((`val` shr 16 and 0xFF) * (1f / 1))
-                    byteBuffer.putFloat((`val` shr 8 and 0xFF) * (1f / 1))
-                    byteBuffer.putFloat((`val` and 0xFF) * (1f / 1))
+                    byteBuffer.putFloat((`val` shr 16 and 0xFF) * (1f / 255f))
+                    byteBuffer.putFloat((`val` shr 8 and 0xFF) * (1f / 255f))
+                    byteBuffer.putFloat((`val` and 0xFF) * (1f / 255f))
                 }
             }
+            val inputFeature0 = TensorBuffer.createFixedSize(
+                intArrayOf(1, imageSize, imageSize, 3),
+                DataType.FLOAT32
+            )
             inputFeature0.loadBuffer(byteBuffer)
-            val outputs: Model.Outputs = model.process(inputFeature0)
-            val outputFeature0: TensorBuffer = outputs.outputFeature0AsTensorBuffer
-            val confidences: FloatArray = outputFeature0.floatArray
-            var maxPos = 0
-            var maxConfidence = 0f
-            for (i in confidences.indices) {
-                if (confidences[i] > maxConfidence) {
-                    maxConfidence = confidences[i]
-                    maxPos = i
-                }
-            }
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+            model.close()
             val classes = arrayOf(
                 "Aluvial",
-                "Andosol",
-                "Entisol",
                 "Humus",
-                "Inceptisol",
-                "Kapur",
-                "Laterit",
-                "pasir"
+                "Laterit"
             )
+            val maxPos =
+                outputFeature0.floatArray.indices.maxByOrNull { outputFeature0.floatArray[it] } ?: 0
             val detectedClass = classes[maxPos]
-            model.close()
             Log.d(TAG, "Image classification result: $detectedClass")
             return detectedClass
         } catch (e: IOException) {
